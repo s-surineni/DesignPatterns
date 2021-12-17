1541e0a92c8802ec7394b370c0e1d7b1d45f72c6
sampath surineni <sampath.surineni@gmail.com>
2021-12-17 22:35:47 +0530

    added sample for builder pattern

---
+++ src/creational/builder/LunchOrderDemo.java
@@ -0,0 +1,13 @@
+package creational.builder;
+
+public class LunchOrderDemo {
+    public static void main(String[] args) {
+        LunchOrder.Builder builder = new LunchOrder.Builder();
+        builder.bread("Wheat").condiments("Lettuce").dressing("Mayo").meat("turkey");
+        LunchOrder lunchOrder = builder.build();
+        System.out.println(lunchOrder.getBread());
+        System.out.println(lunchOrder.getCondiments());
+        System.out.println(lunchOrder.getDressing());
+        System.out.println(lunchOrder.getMeat());
+    }
+}
